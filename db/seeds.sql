dfbae2f3276f634c8872fb0ae1e47a6dc4f8c39f
herivillaz <herivillaz@gmail.com>
2020-08-06 15:30:45 -0400

    create files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Veggie burger');
+INSERT INTO burgers (burger_name) VALUES ('Seafood burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('hawaian burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('New York trash burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Incredible burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Ghost burger');
